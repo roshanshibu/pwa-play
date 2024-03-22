6b4e9b321bced1a5cb7005c86cb376142e497376
Roshan Shibu <roshanshibu97@gmail.com>
2024-03-22 17:32:01 +0100

    add offline handling

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,24 @@
-console.log("hello");
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches
+      .open("static-v1")
+      .then((cache) => cache.addAll(["/offline-1.html", "/dog.svg"]))
+  );
+});
+
 self.addEventListener("fetch", (e) => {
-  const url = new URL(e.request.url);
-  if (url.pathname.endsWith("svg")) {
-    e.respondWith(fetch("/dog.svg"));
+  if (navigator.onLine) {
+    console.log("online");
+    e.respondWith(
+      caches.match(e.request).then((response) => response || fetch(e.request)) // if request is cached, return response from cache, otherwise fetch it from network
+    );
+  } else {
+    console.log("offline");
+    const url = new URL(e.request.url);
+    if (url.pathname.endsWith("svg")) {
+      e.respondWith(fetch("/dog.svg"));
+    } else {
+      e.respondWith(caches.match("/offline-1.html"));
+    }
   }
 });
